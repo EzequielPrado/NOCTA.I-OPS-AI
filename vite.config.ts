47168062356678000e65e78590435ca717495221
Ezequiel Reis <65899925+EzequielPrado@users.noreply.github.com>
2025-06-21 14:08:18 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ops/', // 👈 aqui define o subcaminho da URL
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
